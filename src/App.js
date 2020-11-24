2ef83d6b373dcf5193245055c566e30279dc7636
Kirk <mrincer@MacBook-Pro-de-Kirk.local>
2020-11-23 19:23:06 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Login from './Login';
+import './App.css';
+import { getTokenFromUrl } from './spotify';
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  //Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ""; 
+
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token)
+    }
+  }, []);
+  return (
+    <div className="app">
+
+      {
+        token ? (
+          <h1>Logged In</h1>
+        ) : (
+          <Login />
+        )
+      }
+
+      <Login/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.css';
+import { loginUrl } from './spotify';
+
+function Login()
+{
+    return(
+        <div className="login">
+
+            {/*Spotify Logo*/}
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify Logo"/>
+            
+            {/*Login with Spotify Button*/}
+            <a href={loginUrl}>Login with Spotify</a>
+
+        </div>
+    )
+}
+export default Login;
